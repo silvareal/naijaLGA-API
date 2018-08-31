c95075b0224470f6a7bda59a38d1eba8a9f94f2a
silvareal <sylvernusakubo@yahoo.com>
2018-08-31 00:45:12 -0700

    admin rest url

--- state/urls.py
+++ state/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import include, url
 from rest_framework.routers import SimpleRouter
+from rest_framework_swagger.views import get_swagger_view
 
+from django.urls import path, include
 from state import views
+
+
+API_TITLE = 'NG states and LGA API'
+schema_view = get_swagger_view(title=API_TITLE )
 
 # Create a router and register our viewsets with it.
 router = SimpleRouter()
@@ -11,5 +17,9 @@ router.register(r'stateslga', views.StateLGAViewSet)
 # The API URLs are now determined  automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
-    url(r'^v1/', include(router.urls))
+    url(r'^api/v1/', include(router.urls)),
+
+    #api doc schema
+    path(r'', schema_view),
+    url(r'^api/', include('rest_framework.urls')),
 ]
--- statesapi/urls.py
+++ statesapi/urls.py
@@ -1,25 +1,10 @@
 
 from django.contrib import admin
 from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
-from django.views.generic import RedirectView
 
 from django.urls import path, include
 
-
-API_TITLE = 'NG states and LGA API'
-schema_view = get_swagger_view(title=API_TITLE )
-
 urlpatterns = [
     path('state-admin/', admin.site.urls),
-    url('api/', include('state.urls')),
-    url(r'^api-auth/', include('rest_framework.urls')),
-
-    #api doc schema
-    url(r'^docs/$', schema_view),
-]
-
-
-urlpatterns += [
-    url(r'^$', RedirectView.as_view(url="/docs/", permanent=True)),
+    path('', include('state.urls')),
 ]
